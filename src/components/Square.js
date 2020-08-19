58db42d4a472ff95cc6edf69cfa56d9cb86bdd59
ablades <audaris@gmail.com>
2020-08-19 13:45:43 -0500

    add grid, create square

---
+++ src/components/Square.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Square() {
+    const styles = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'red',
+    }
+
+    return(
+        <div className="square" style={styles}>
+        </div>
+    )
+}
+
+export default Square
